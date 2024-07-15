2b2401bf879e6c5f6085c47809494f594bd27d9f
Mehmet Fatih Durna <mfd304603@gmail.com>
2024-07-15 15:36:25 +0300

    Project was created

---
+++ src/components/MortgageRepaymentCalculator.jsx
@@ -0,0 +1,15 @@
+import styles from '../assets/styles/MortgageRepaymentCalculator.module.css';
+
+export default function MortgageRepaymentCalculator() {
+  return (
+    <div className={styles.container}>
+        <div >
+
+        </div>
+
+        <div>
+
+        </div>
+    </div>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom/client'
+import {BrowserRouter, Route, Routes} from'react-router-dom'
+import App from './App.jsx'
+import MortgageRepaymentCalculator from './components/MortgageRepaymentCalculator.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path='/mortgage-repayment-calculator' element={<MortgageRepaymentCalculator/>}></Route>
+    </Routes>
+  </BrowserRouter>
+)
