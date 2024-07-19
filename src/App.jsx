7b5c1a6c0d76997a2400a3cbb9b9248bb56f41eb
Mehmet Fatih Durna <mfd304603@gmail.com>
2024-07-19 16:51:58 +0300

    netlify config dosyası eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import {BrowserRouter, Route, Routes} from'react-router-dom'
+import MortgageRepaymentCalculator from './components/MortgageRepaymentCalculator'
+import HomePage from './components/HomePage';
+import NoPage from './components/NoPage';
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/mortgage-repayment-calculator" element={<MortgageRepaymentCalculator />} />
+      <Route path='/' element={<HomePage/>}></Route>
+      <Route path="*" element={<NoPage />} />
+    </Routes>
+  </BrowserRouter>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import ReactDOM from 'react-dom/client'
-import {BrowserRouter, Route, Routes} from'react-router-dom'
+import React from 'react'
 import App from './App.jsx'
-import MortgageRepaymentCalculator from './components/MortgageRepaymentCalculator.jsx'
 import "./index.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path='/mortgage-repayment-calculator' element={<MortgageRepaymentCalculator/>}></Route>
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <App></App>
+  </React.StrictMode>
 )
